face783a3e5098fccb87deb2db656e14fc59b1bf
Zuruh <ziadi.mail.pro@gmail.com>
2021-10-15 11:33:08 +0200

    ✨ Created fake data

---
+++ src/data/data.ts
@@ -0,0 +1,34 @@
+import type { Channel } from "../types/types";
+
+export const channels = [
+  {
+    id: 1,
+    name: "Sports",
+    streamers: ["Ponce", "Kameto", "Domingo", "Gotaga"],
+  },
+  {
+    id: 2,
+    name: "Jeux Vidéos",
+    streamers: ["Ponce", "Kameto", "Domingo", "Gotaga"],
+  },
+  {
+    id: 3,
+    name: "Just Chatting",
+    streamers: ["Ponce", "Kameto", "Domingo", "Gotaga"],
+  },
+  {
+    id: 4,
+    name: "Arts",
+    streamers: ["Ponce", "Kameto", "Domingo", "Gotaga"],
+  },
+  {
+    id: 5,
+    name: "Musique",
+    streamers: ["Ponce", "Kameto", "Domingo", "Gotaga"],
+  },
+  {
+    id: 6,
+    name: "Sports",
+    streamers: ["Ponce", "Kameto", "Domingo", "Gotaga"],
+  },
+] as Channel[];
