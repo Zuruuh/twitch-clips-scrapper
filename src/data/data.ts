a9533c933d2e93c0c593baffa77392c3bb7e05cc
Zuruh <ziadi.mail.pro@gmail.com>
2021-10-15 18:57:04 +0200

    🔥 Removed useless fake data

--- src/data/data.ts
+++ src/data/data.ts
@@ -1,34 +1,7 @@
-import type { Channel } from "../types/types";
+import type { Channel as ChannelType } from "../types/types";
 
-export const channels = [
-  {
-    id: 1,
-    name: "Sports",
-    streamers: ["Ponce", "Kamet0", "Domingo", "Gotaga"],
-  },
-  {
-    id: 2,
-    name: "Jeux Vidéos",
-    streamers: ["Ponce", "Kamet0", "Domingo", "Gotaga"],
-  },
-  {
-    id: 3,
-    name: "Just Chatting",
-    streamers: ["Ponce", "Kamet0", "Domingo", "Gotaga"],
-  },
-  {
-    id: 4,
-    name: "Arts",
-    streamers: ["Ponce", "Kamet0", "Domingo", "Gotaga"],
-  },
-  {
-    id: 5,
-    name: "Musique",
-    streamers: ["Ponce", "Kamet0", "Domingo", "Gotaga"],
-  },
-  {
-    id: 6,
-    name: "Sports",
-    streamers: ["Ponce", "Kamet0", "Domingo", "Gotaga"],
-  },
-] as Channel[];
+export const Channel = {
+  name: "*",
+  streamers: ["Ponce", "Kamet0", "Domingo", "Gotaga"],
+  videos: [],
+} as ChannelType;
